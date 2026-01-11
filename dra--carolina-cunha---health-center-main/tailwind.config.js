cb8ebba9bda18439dc082d943c173a28fb313189
DevWeb <devwebofc@gmail.com>
2026-01-11 11:31:14 -0300

    Update dra--carolina-cunha---health-center-main/tailwind.config.js via Orchestrator

---
+++ dra--carolina-cunha---health-center-main/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+        theme: {
+          extend: {
+            colors: {
+              brown: {
+                800: '#4A3B32', // Deep sophisticated brown
+                900: '#2C221C',
+              },
+              gold: {
+                400: '#C5A059',
+                500: '#B08D55',
+                600: '#907242',
+              },
+              beige: {
+                50: '#F9F8F6',
+                100: '#F2EFE9',
+              },
+            },
+            fontFamily: {
+              serif: ['"Cormorant Garamond"', 'serif'],
+              sans: ['"Montserrat"', 'sans-serif'],
+            },
+          },
+        },
+      };
